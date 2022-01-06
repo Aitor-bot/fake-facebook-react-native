16edbac7379ad3c9bc9cbb89132acce15483b089
aitor.palleja <aitor.palleja@ikasle.aeg.eus>
2022-01-07 00:34:23 +0100

    Navigation done

---
+++ screens/Groups.js
@@ -0,0 +1,22 @@
+import React from 'react'
+import styled from 'styled-components/native'
+
+import ToolBar from '../components/ToolBar';
+import MainSeparator from '../components/MainSeparator';
+import Users from '../components/Users';
+import Story from '../components/Story';
+import Post from '../components/Post';
+
+const View = styled.View`
+    align-items: center;
+    height: 500;
+`;
+
+const Groups = () => {
+    return(
+        <View>
+            <Text>Screen</Text>
+        </View>
+    )
+}
+export default Groups
