6b069d92bf53b02fd3f45ff08dc9979f2c8d67cf
aitor.palleja <aitor.palleja@ikasle.aeg.eus>
2022-01-07 00:39:46 +0100

    Screens import fixed

--- screens/Menu.js
+++ screens/Menu.js
@@ -1,11 +1,7 @@
 import React from 'react'
+import { Text } from 'react-native'
 import styled from 'styled-components/native'
 
-import ToolBar from '../components/ToolBar';
-import MainSeparator from '../components/MainSeparator';
-import Users from '../components/Users';
-import Story from '../components/Story';
-import Post from '../components/Post';
 
 const View = styled.View`
     align-items: center;
