fd64933be68904a29431b547651e8468ed35c0f7
aitor.palleja <aitor.palleja@ikasle.aeg.eus>
2022-01-07 01:14:44 +0100

    Home II done

--- components/Avatar.js
+++ components/Avatar.js
@@ -12,12 +12,27 @@ const ProfileImage = styled.Image`
     width: 40px;
     height: 40px;
     border-radius: 20px;
+    border-color: ${(props => props.checked ? '#FFFFFF' : '#1777f2')};
+    border-width: ${(props => props.story ? '3px' : 0)};
 `
 
-const Avatar = ({source}) => {
+const UserActive = styled.View`
+    position: absolute;
+    bottom: -2px;
+    right: -2px;
+    width: 15px;
+    height: 15px;
+    border-radius: 8px;
+    background: #10D24b;
+    border: 2px solid #FFFFFF;
+`
+
+
+const Avatar = ({source, online, story, checked}) => {
     return (
         <Container>
             <ProfileImage source={source}/>
+            {online && <UserActive />}
         </Container>
     )
 }
--- screens/Home.js
+++ screens/Home.js
@@ -1,8 +1,10 @@
 import React from 'react'
 import { Text } from 'react-native'
 import styled from 'styled-components/native'
+
 import MainSeparator from '../components/MainSeparator';
 import ToolBar from '../components/ToolBar';
+import Users from '../components/Users';
 
 const View = styled.View`
     flex: 1;
@@ -12,6 +14,8 @@ const Home = () => {
     return(
         <View>
             <ToolBar/>
+            <MainSeparator />
+            <Users />
             <MainSeparator />
         </View>
     )
